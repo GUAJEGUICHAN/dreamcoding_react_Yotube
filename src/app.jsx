62d1ac2da4c810d50a86f6719a2221ef54ef77a8
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-09-07 15:51:11 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './app.css';
-
+import VideoList from './components/video_list';
 function App() {
+
+  const [ videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC42CAmiPOT9way8qwzgsfFC0PTcA6BzM0", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[])
   return (
-    <h1> HI </h1>
+    <VideoList videos = {videos} />
   );
 }
 
